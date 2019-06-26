30190e676c8857a8f3dfdd99e012ac235533275e
Charbonnier Robin <charbonnierrobin@gmail.com>
2019-06-26 17:35:38 +0200

    Home page et liste voiture

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './component/home-page/home-page.component';
+import { FormHomePageComponent } from './component/form-home-page/form-home-page.component';
+import { VehiculeListeComponent } from './component/vehicule-liste/vehicule-liste.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    FormHomePageComponent,
+    VehiculeListeComponent
   ],
   imports: [
     BrowserModule,
---
+++ Frontend/src/app/component/vehicule-liste/vehicule-liste.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicule-liste',
+  templateUrl: './vehicule-liste.component.html',
+  styleUrls: ['./vehicule-liste.component.css']
+})
+export class VehiculeListeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
