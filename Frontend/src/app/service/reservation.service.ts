a41faff5b96743d5193b48349c9a4bdebbcc8e05
thomas <thomas_almeida@hotmail.fr>
2019-06-28 11:10:17 +0200

    modification backend + ajout service reservation et composer reservation

---
+++ Frontend/src/app/service/reservation.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {  HttpErrorResponse, HttpParams, HttpClientModule } from '@angular/common/http';
+import { Reservation } from 'src/app/class/reservation';
+import { Marque } from 'src/app/class/marque';
+import { HttpClient } from '@angular/common/http';
+import { map, catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReservationService {
+ // marque = Marque[]
+  tab = [];
+  baseUrl = 'http://localhost/api';
+  reservation: Reservation[];
+  
+  constructor(private http: HttpClient) { }
+  
+    getAll() {
+      console.log("trouver2");
+      return this.http.get('http://localhost/api/script.php');
+    }
+}
