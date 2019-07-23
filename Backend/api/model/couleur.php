914e6aceecc002fbc096a4d4c82206e7701557ba
thomas <thomas_almeida@hotmail.fr>
2019-07-23 21:53:15 +0200

    ajout liste marque, model, type pour recherche

--- Backend/api/model/couleur.php
+++ Backend/api/model/couleur.php
@@ -3,7 +3,18 @@ require_once('config/config.php');
 
 class couleur extends config
 {
-    function getAll(){}
+    function getAll(){
+        try {
+            $data_base=$this->connection();
+            $select = $data_base->prepare("Select couleur.id, couleur.nom From couleur");
+            $select->execute();
+            $data=$select->fetchAll();
+            return $data;
+        } catch (PDOException $e) {
+            return "Erreur !: " . $e->getMessage() . "<br/>";
+            die();
+        }
+	}
     
     function insert(){}
     
--- Frontend/src/app/component/reservation/reservation.component.ts
+++ Frontend/src/app/component/reservation/reservation.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Reservation } from 'src/app/class/reservation';
 import { Marque } from 'src/app/class/marque';
 import { ReservationService } from 'src/app/service/reservation.service';
+import { MarqueService } from 'src/app/service/marque.service';
+import { ModelService } from 'src/app/service/model.service';
+import { TypeService } from 'src/app/service/type.service';
+import { CouleurService } from 'src/app/service/couleur.service';
 
 
 @Component({
@@ -11,28 +15,38 @@ import { ReservationService } from 'src/app/service/reservation.service';
 })
 export class ReservationComponent implements OnInit {
 
-  constructor(private ReservationService: ReservationService) {}
-  type_vehicule = ['scooter','voiture']
-  marque: Marque[]
-  model = ['models', 'modelx'];
-  couleur = ['rouge', 'bleu', 'vert', 'noir'];
-  type = ['4x4', 'couper', 'cabriolet', 'sport'];
+  constructor(
+    private ReservationService: ReservationService,
+    private ModelService: ModelService,
+    private MarqueService: MarqueService,
+    private CouleurService: CouleurService,
+    private TypeService: TypeService
+    ) {}
 
-  getMarque(): void {
-    console.log("trouver");
-    
+  type_vehicule: any[];
+  marque: Marque[]
+  model: any[];
+  couleur: any[];
+  type: any[];
+
+  Onchange(marque) {
+    this.model = this.ModelService.getAll(marque);
   }
 
   ngOnInit() {
-    this.marque = this.ReservationService.getAll();
-    console.log(this.marque);
+    this.marque = this.MarqueService.getAll();
+    this.couleur = this.CouleurService.getAll();
+    this.type_vehicule = this.TypeService.getAll();
   }
 
+  Onclick() {
+
+  }
  
 
   
 
- reservation = new Reservation(null,null,null,null,null,null,this.model[0],this.couleur[0],this.type[0]);
+ reservation = new Reservation(null,null,null,null,null,null,null,null,null);
 
   submitted = false;
 
